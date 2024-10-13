d6b1ddaf2febf742998ddfa97cc9ac76c26bf5ec
ParikshithLakshmikanth <132085131+ParikshithLakshmikanth@users.noreply.github.com>
2024-10-13 17:55:00 +0530

    calci

---
+++ main.py
@@ -0,0 +1,16 @@
+def add(x,y):
+    print(x+y)
+def sub(x,y):
+    print(x-y)
+def mul(x,y):
+    print(x*y)
+def div(x,y):
+    print(x/y)
+
+a = int(input("Enter a number: "))
+b= int(input("Enter another number: "))
+
+add(a,b)
+sub(a,b)
+mul(a,b)
+div(a,b)
